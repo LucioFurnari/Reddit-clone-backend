35acd7d4df6de2df4ed37f6ed9b4677b5aaf2fdf
LucioFurnari <luciofurnari99@gmail.com>
2024-11-26 22:58:14 -0300

    feat: add auth route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth';
 
 dotenv.config();
 
@@ -8,9 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, Reddit Clone!');
-});
+app.use('/api', authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
