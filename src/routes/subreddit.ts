be879a83e52f7d981a95ca27c4cbe3854193d41d
LucioFurnari <luciofurnari99@gmail.com>
2024-12-01 23:35:58 -0300

    feat: add subreddit routes

---
+++ src/routes/subreddit.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { createSubReddit, getAllSubReddit, getSubReddit } from "../controllers/subreddit";
+
+const router = Router();
+
+// Create Subreddit
+router.post('/subreddit', createSubReddit);
+
+// Get all Subreddit
+router.get('/subreddit', getAllSubReddit);
+
+// Get Subreddit by id
+router.get('/subreddit/:id', getSubReddit);
