b757c5d7247b525e073b35fed4e54e096d925c98
LucioFurnari <luciofurnari99@gmail.com>
2024-12-10 16:02:29 -0300

    feat: import vote route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import subRedditRoutes from './routes/subreddit';
+import voteRoutes from './routes/vote';
 
 dotenv.config();
 
@@ -12,7 +13,7 @@ app.use(express.json());
 
 app.use('/api', authRoutes);
 app.use('/api', subRedditRoutes);
-
+app.use('/api', voteRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
