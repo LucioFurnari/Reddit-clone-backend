69e212400e0f7f278d9688e265240d61680150c7
LucioFurnari <luciofurnari99@gmail.com>
2024-12-16 21:33:24 -0300

    feat: add edit route

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken } from "../controllers/auth";
-import { createPost, getPostById, getSubRedditPosts } from "../controllers/post";
+import { createPost, getPostById, getSubRedditPosts, editPost, deletePost } from "../controllers/post";
 
 const router = Router();
 
@@ -12,5 +12,8 @@ router.get("/subreddit/posts/:postId", getPostById);
 
 // Get all posts by subreddit
 router.get("/subreddit/:subredditId/posts", getSubRedditPosts);
+
+// Edit post by id
+router.put("/posts/:postId", editPost);
 
 export default router;
