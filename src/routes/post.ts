c04d4a2175389c2148980cb0a8c32dddc7ef8483
LucioFurnari <luciofurnari99@gmail.com>
2024-12-16 21:25:23 -0300

    feat: add get posts routes

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { authenticateToken } from "../controllers/auth";
-import { createPost } from "../controllers/post";
+import { createPost, getPostById, getSubRedditPosts } from "../controllers/post";
 
 const router = Router();
 
 // Create post
 router.post("/subreddit/:subredditId/posts", authenticateToken, createPost);
+
+// Get post by id
+router.get("/subreddit/posts/:postId", getPostById);
+
+// Get all posts by subreddit
+router.get("/subreddit/:subredditId/posts", getSubRedditPosts);
 
 export default router;
