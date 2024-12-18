6932867eeca494c6d3febed3a98022756c78d138
LucioFurnari <luciofurnari99@gmail.com>
2024-12-18 19:46:54 -0300

    feat: add edit comment route

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createComment, getComments } from "../controllers/comment";
+import { createComment, getComments, editComment } from "../controllers/comment";
 
 const router = Router();
 
@@ -8,5 +8,8 @@ router.post("/posts/:postId/comments", createComment);
 
 // Get comments
 router.get("/posts/:postId/comments", getComments);
+
+// Edit comment
+router.put("/comments/:commentId", editComment);
 
 export default router;
