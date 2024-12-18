9b890aceb186922b10b237d2c4381c52ef2aa7a1
LucioFurnari <luciofurnari99@gmail.com>
2024-12-18 20:27:06 -0300

    feat: add authenticate token midleware

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { createComment, getComments, editComment } from "../controllers/comment";
+import { authenticateToken } from "../controllers/auth";
 
 const router = Router();
 
 // Create comment
-router.post("/posts/:postId/comments", createComment);
+router.post("/posts/:postId/comments", authenticateToken, createComment);
 
 // Get comments
 router.get("/posts/:postId/comments", getComments);
 
 // Edit comment
-router.put("/comments/:commentId", editComment);
+router.put("/comments/:commentId", authenticateToken, editComment);
 
 export default router;
