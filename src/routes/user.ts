b2e0887d2764ceb2a5707b1571c4b2835becd659
LucioFurnari <luciofurnari99@gmail.com>
2024-12-23 21:01:10 -0300

    feat: add route for get subscribed subreddits

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { updateUserProfile } from "../controllers/user";
+import { updateUserProfile, getSubscribedSubreddits } from "../controllers/user";
 import { authenticateToken } from "../controllers/auth";
 
 const router = Router();
 
+// Update profile
 router.put("/profile", authenticateToken, updateUserProfile);
+
+// Get subscribed subreddits
+router.get("/subscribed-subreddits", authenticateToken, getSubscribedSubreddits);
 
 export default router;
