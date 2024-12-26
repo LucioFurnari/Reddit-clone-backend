2768791bb963db3d9d98ecc47b54f7d5f172e419
LucioFurnari <luciofurnari99@gmail.com>
2024-12-26 15:44:03 -0300

    feat: add search subreddit route

--- src/routes/subreddit.ts
+++ src/routes/subreddit.ts
@@ -1,22 +1,24 @@
 import { Router } from "express";
-import { createSubReddit, getAllSubReddit, getSubReddit, subscribeToSubreddit, unsubscribeFromSubreddit } from "../controllers/subreddit";
+import { createSubReddit, getAllSubReddit, getSubReddit, subscribeToSubreddit, unsubscribeFromSubreddit, searchSubreddits } from "../controllers/subreddit";
 import { authenticateToken } from "../controllers/auth";
 
 const router = Router();
 
 // Create Subreddit
-router.post('/subreddit', authenticateToken, createSubReddit);
+router.post("/subreddit", authenticateToken, createSubReddit);
 
 // Get all Subreddit
-router.get('/subreddit', getAllSubReddit);
+router.get("/subreddit", getAllSubReddit);
 
 // Get Subreddit by id
-router.get('/subreddit/:subredditId', getSubReddit);
+router.get("/subreddit/:subredditId", getSubReddit);
 
 // Subscribe to a subreddit
-router.post('/subreddit/:subredditId/subscribe', authenticateToken, subscribeToSubreddit);
+router.post("/subreddit/:subredditId/subscribe", authenticateToken, subscribeToSubreddit);
 
 // Unsubscribe from a subreddit
-router.delete('/subreddit/:subredditId/unsubscribe', authenticateToken, unsubscribeFromSubreddit);
+router.delete("/subreddit/:subredditId/unsubscribe", authenticateToken, unsubscribeFromSubreddit);
 
+// Search subreddit by name
+router.get("/subreddits/search", searchSubreddits);
 export default router;
