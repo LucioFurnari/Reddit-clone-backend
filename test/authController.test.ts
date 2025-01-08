10835f6c4ba073c257ef8e08915265e1bd9435e1
LucioFurnari <luciofurnari99@gmail.com>
2025-01-08 16:41:08 -0300

    feat: change function to reset mock

--- test/authController.test.ts
+++ test/authController.test.ts
@@ -5,6 +5,9 @@ import { v4 as uuidv4 } from "uuid";
 
 describe("POST /api/signup", () => {
   it("Should create a new user", async () => {
+    beforeEach(() => {
+      prismaMock.user.create.mockReset(); // Reset mocks before each test
+    });
     // Mock prisma create method
     const mockUser = {
       id: 'some-uuid',
@@ -22,7 +25,7 @@ describe("POST /api/signup", () => {
     const res = await request(app)
       .post('/api/signup')
       .send({
-        email: 'bastio74@gmail.com',
+        email: 'bastio174@gmail.com',
         username: 'Bastio',
         password: 'clave741123',
       });
@@ -32,3 +35,32 @@ describe("POST /api/signup", () => {
     expect(res.body).toHaveProperty('message', 'User created');
   });
 });
+
+// describe("POST /api/login", () => {
+//   it("Should login a user", async () => {
+//     // Mock prisma create method
+//     const mockUser = {
+//       id: 'some-uuid',
+//       email: 'bastio74@gmail.com',
+//       username: 'Bastio',
+//       password: '12345678',
+//       createdAt: new Date(),
+//       profilePictureUrl: null,
+//       bio: null,
+//     };
+
+//     prismaMock.user.create.mockResolvedValue(mockUser);
+
+//     // Make a POST request to login the user
+//     const res = await request(app)
+//     .post("/api/login")
+//     .send({
+//       email: "bastio74@gmail.com",
+//       password: "12345678"
+//     });
+
+//     // Assert the response
+//     expect(res.status).toBe(200);
+//     expect(res.body).toHaveProperty("message", "Logged in successfully");
+//   });
+// });
