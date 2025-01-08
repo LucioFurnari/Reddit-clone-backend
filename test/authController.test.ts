e57152f3072dcce1dc4d2d2bb6b39192aef8c280
LucioFurnari <luciofurnari99@gmail.com>
2025-01-08 01:14:52 -0300

    feat: change testing functioNg

--- test/authController.test.ts
+++ test/authController.test.ts
@@ -3,26 +3,32 @@ import prismaMock from "./prismaMock";
 import { app } from "../src/app";
 import { v4 as uuidv4 } from "uuid";
 
-describe("POST /signup", () => {
+describe("POST /api/signup", () => {
   it("Should create a new user", async () => {
     // Mock prisma create method
-    const id = uuidv4();
-    prismaMock.user.create.mockResolvedValue({
-      id: id,
-      email: "bastio74@gmail.com",
-      username: "Bastio",
-      password: "123",
+    const mockUser = {
+      id: 'some-uuid',
+      email: 'bastio74@gmail.com',
+      username: 'Bastio',
+      password: '123',
       createdAt: new Date(),
       profilePictureUrl: null,
-      bio: null
-    });
+      bio: null,
+    };
+
+    prismaMock.user.create.mockResolvedValue(mockUser);
 
     // Make a POST request to create a new user
-    const res = await request(app).post("/signup")
+    const res = await request(app)
+      .post('/api/signup')
+      .send({
+        email: 'bastio74@gmail.com',
+        username: 'Bastio',
+        password: 'clave741123',
+      });
 
     // Assert the response
     expect(res.status).toBe(201);
-    expect(res.body).toHaveProperty("message");
-    expect(res.body.message).toBe("User created");
+    expect(res.body).toHaveProperty('message', 'User created');
   });
 });
