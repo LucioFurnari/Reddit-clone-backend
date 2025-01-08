f82b78135730d1acb4a44687bdb400d28aaaa5ca
LucioFurnari <luciofurnari99@gmail.com>
2025-01-08 18:59:46 -0300

    feat: add test for login user

--- test/authController.test.ts
+++ test/authController.test.ts
@@ -1,4 +1,5 @@
 import request from "supertest";
+import bcrypt from "bcrypt";
 import prismaMock from "./prismaMock";
 import { app } from "../src/app";
 import { v4 as uuidv4 } from "uuid";
@@ -36,31 +37,32 @@ describe("POST /api/signup", () => {
   });
 });
 
-// describe("POST /api/login", () => {
-//   it("Should login a user", async () => {
-//     // Mock prisma create method
-//     const mockUser = {
-//       id: 'some-uuid',
-//       email: 'bastio74@gmail.com',
-//       username: 'Bastio',
-//       password: '12345678',
-//       createdAt: new Date(),
-//       profilePictureUrl: null,
-//       bio: null,
-//     };
+describe("POST /api/login", () => {
+  it("Should login a user", async () => {
+    const plainPassword = "password123";
+    const hashedPassword = await bcrypt.hash(plainPassword, 10);
 
-//     prismaMock.user.create.mockResolvedValue(mockUser);
+    // Mock prisma create method
+    const mockUser = {
+      id: 'some-uuid',
+      email: 'bastio74@gmail.com',
+      username: 'Bastio',
+      password:  hashedPassword,
+      createdAt: new Date(),
+      profilePictureUrl: null,
+      bio: null,
+    };
 
-//     // Make a POST request to login the user
-//     const res = await request(app)
-//     .post("/api/login")
-//     .send({
-//       email: "bastio74@gmail.com",
-//       password: "12345678"
-//     });
+    prismaMock.user.findUnique.mockResolvedValue(mockUser);
 
-//     // Assert the response
-//     expect(res.status).toBe(200);
-//     expect(res.body).toHaveProperty("message", "Logged in successfully");
-//   });
-// });
+     // Make a POST request to login the user
+    const res = await request(app).post("/api/login").send({
+      email: "bastio74@gmail.com",
+      password: plainPassword,
+    });
+
+    // Assert the response
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("message", "Logged in successfully");
+  });
+});
