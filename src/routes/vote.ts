152f65bca54148c42f2d74825e22cf849590f65d
LucioFurnari <luciofurnari99@gmail.com>
2025-03-15 16:23:31 -0300

    feat: add routes for remove votes

--- src/routes/vote.ts
+++ src/routes/vote.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { vote } from "../controllers/voteController";
+import { vote, removeVote } from "../controllers/voteController";
 import { authenticateToken } from "../controllers/auth";
 
 const router = Router();
 
 // Voting route
 router.patch("/vote", authenticateToken, vote);
+
+// Remove post vote
+router.delete("/vote/:postId", authenticateToken, removeVote);
+
+// Remove comment vote
+router.delete("/vote/:commentId", authenticateToken, removeVote);
 
 export default router;
