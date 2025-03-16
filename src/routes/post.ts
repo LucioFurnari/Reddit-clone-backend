c7ea142bf1a6061e4c2a5d836bbf7d5ef136d3ce
LucioFurnari <luciofurnari99@gmail.com>
2025-03-16 00:04:23 -0300

    feat: add routes for get posts

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken } from "../controllers/auth";
-import { createPost, getPostById, getSubRedditPosts, editPost, deletePost, searchPosts } from "../controllers/post";
+import { createPost, getPostById, getSubRedditPosts, editPost, deletePost, searchPosts, getPosts, getSubscribedPosts, } from "../controllers/post";
 
 const router = Router();
 
@@ -9,6 +9,12 @@ router.post("/subreddits/:subredditId/posts", authenticateToken, createPost);
 
 // Search posts by name
 router.get("/posts/search", searchPosts);
+
+// Get 10 posts
+router.get("/posts", getPosts);
+
+// Get subscribed posts
+router.get("/posts/subscribed", authenticateToken, getSubscribedPosts);
 
 // Get post by id
 router.get("/posts/:postId", getPostById);
@@ -21,5 +27,6 @@ router.put("/posts/:postId", authenticateToken, editPost);
 
 // Delete post by id
 router.delete("/posts/:postId", authenticateToken, deletePost);
+
 
 export default router;
